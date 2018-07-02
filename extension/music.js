3d6ba4de7288c394d8eb62ac0a8798d79604fd16
zoton2 <zoton2@gmail.com>
2018-07-02 12:43:33 +0100

    MPD random/shuffle workaround

--- extension/music.js
+++ extension/music.js
@@ -43,13 +43,17 @@ function onReady() {
 		if (status.playlistlength <= 0) {
 			nodecg.log.info('Doing initial MPD configuration.');
 			client.sendCommand('add /');    // Add all songs to play queue.
-			client.sendCommand('random 1'); // Set player to random mode.
 			client.sendCommand('repeat 1'); // Set player to repeat.
+			shufflePlaylist();              // Shuffle the music.
 			client.sendCommand('play');     // Play songs.
 		}
 
 		// Always set volume on connection just in case, but we need to wait a little for some reason (probably for playback to commence).
-		setTimeout(() => client.sendCommand('setvol '+volume), 2000);
+		setTimeout(setVolume, 2000);
+
+		// Shuffle the playlist every 6 hours.
+		// (We're only playing music in intermissions; doesn't need to be frequent).
+		setInterval(shufflePlaylist, 21600000);
 
 		updatePlaybackStatus();
 		updateCurrentSong();
@@ -103,4 +107,17 @@ function toggleSongPlayback(pause) {
 // Can be used to skip to the next song.
 function skipSong() {
 	client.sendCommand('next');
+}
+
+// Used to shuffle the currently playing list *correctly*.
+// Actual shuffle is the same *every time* so let's add some randomness here!
+function shufflePlaylist() {
+	var random = Math.floor(Math.random()*Math.floor(20));
+	for (var i = 0; i < random; i++)
+		client.sendCommand('shuffle');
+}
+
+// Used to set the player volume to whatever the variable is set to.
+function setVolume() {
+	client.sendCommand('setvol '+volume)
 }
