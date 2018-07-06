2935ff238f53d9c98151cd552f22c6e392209060
zoton2 <zoton2@gmail.com>
2018-07-06 03:15:02 +0100

    Intermission layout to add/test music control

---
+++ graphics/js/music.js
@@ -0,0 +1,13 @@
+'use strict';
+$(() => {
+	// The bundle name where all the run information is pulled from.
+	var speedcontrolBundle = 'nodecg-speedcontrol';
+
+	// Stops, then starts a highlight if layout becomes active in OBS. Also cuts the upload recording.
+	window.obsstudio.onActiveChange = function(active) {
+		if (active)
+			nodecg.sendMessage('playSong');
+		else
+			nodecg.sendMessage('pauseSong');
+	};
+});
