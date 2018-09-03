2952a831a9211cc964a3a755e194121692d9c510
zoton2 <zoton2@gmail.com>
2018-09-03 21:19:21 +0100

    Changing some colours/images

--- graphics/js/omnibar/esa-logo.js
+++ graphics/js/omnibar/esa-logo.js
@@ -11,15 +11,15 @@ function changeESALogo() {
 	
 	esaBarLogoTicks++;
 	
-	// Change to hashtag text after 50s.
-	if (esaBarLogoCurrentRotation === 0 && esaBarLogoTicks >= 11) {
+	// Change to hashtag text after 30s.
+	if (esaBarLogoCurrentRotation === 0 && esaBarLogoTicks >= 7) {
 		animationFadeOutInElements(smallLogo, hashtag);
 		esaBarLogoTicks = 1;
 		esaBarLogoCurrentRotation ^= 1; // Toggle between 0 and 1.
 	}
 	
-	// Change to normal logo after 10s.
-	else if (esaBarLogoCurrentRotation === 1 && esaBarLogoTicks >= 3) {
+	// Change to normal logo after 30s.
+	else if (esaBarLogoCurrentRotation === 1 && esaBarLogoTicks >= 7) {
 		animationFadeOutInElements(hashtag, smallLogo);
 		esaBarLogoTicks = 1;
 		esaBarLogoCurrentRotation ^= 1; // Toggle between 0 and 1.
